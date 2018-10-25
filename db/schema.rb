64aa74152f11cb3ee0f1db98d24fb42fa53623fa
Al Barleta <al.barleta@gmail.com>
2018-10-25 10:14:18 +0000

    Create Attendance Table and many-to-many association between the attendee and the event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_094614) do
+ActiveRecord::Schema.define(version: 2018_10_25_100845) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
