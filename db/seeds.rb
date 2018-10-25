b28dc9b31936ebb09e4b152f0c8e7d5b77acc721
Al Barleta <al.barleta@gmail.com>
2018-10-25 10:25:52 +0000

    Make the Event’s Show page display a list of attendees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
-User.create!(username: "cylinderseal", email: "al@example.com", 
+al = User.create!(username: "cylinderseal", email: "al@example.com", 
              password: "123456", password_confirmation: "123456")
-             
+brad = User.create!(username: "brad", email: "brad@example.com", 
+             password: "123456", password_confirmation: "123456")
+alex = User.create!(username: "alex", email: "alex@example.com", 
+             password: "123456", password_confirmation: "123456")
+
+event1 = Event.create!(title: "Ruby Party", description: "Hello, world", creator_id: 1)
+event2 = Event.create!(title: "Ripple Party", description: "Hello, world", creator_id: 2)
+event3 = Event.create!(title: "XRP Party", description: "Hello, world", creator_id: 3)
+
+event1.attendees << al
+event2.attendees << al
+event2.attendees << alex
+event2.attendees << brad
+event3.attendees << al
+event3.attendees << alex
